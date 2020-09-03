7cfbdfabe5327f4d24fcfe984708228234f1033c
zouguoyin <zouguoyin@qq.com>
2020-09-03 16:39:24 +0800

    rocketmq服务关闭脚本

---
+++ rocketmq-stop.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#########################################################################
+# File Name: rocketmq-stop.sh
+# Author: zouguoyin
+# mail: 995637339@qq.com
+# Created Time: 2020年09月03日 星期三 16时27分48秒
+#########################################################################
+sh mqshutdown namesrv
+sh mqshutdown broker
